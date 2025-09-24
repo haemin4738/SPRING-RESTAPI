644f6fca32208d1b397610759ba230385efccf73
haemin4738 <leeheamin12@gmail.com>
2025-09-24 14:05:07 +0900

    prettierrc 설정

--- frontend/src/lib/backend/apiV1/schema.d.ts
+++ frontend/src/lib/backend/apiV1/schema.d.ts
@@ -240,13 +240,13 @@ export interface components {
         };
         PostCommentDto: {
             /** Format: int64 */
-            id?: number;
+            id: number;
             /** Format: date-time */
-            createDate?: string;
+            createDate: string;
             /** Format: date-time */
-            modifyDate?: string;
-            authorName?: string;
-            content?: string;
+            modifyDate: string;
+            authorName: string;
+            content: string;
         };
         RsDataPostCommentDto: {
             resultCode?: string;
@@ -260,12 +260,12 @@ export interface components {
         };
         MemberDto: {
             /** Format: int64 */
-            id?: number;
+            id: number;
             /** Format: date-time */
-            createDate?: string;
+            createDate: string;
             /** Format: date-time */
-            modifyDate?: string;
-            nickname?: string;
+            modifyDate: string;
+            nickname: string;
         };
         RsDataMemberDto: {
             resultCode?: string;
@@ -277,9 +277,9 @@ export interface components {
             password: string;
         };
         MemberLoginResBody: {
-            item?: components["schemas"]["MemberDto"];
-            apiKey?: string;
-            accessToken?: string;
+            item: components["schemas"]["MemberDto"];
+            apiKey: string;
+            accessToken: string;
         };
         RsDataMemberLoginResBody: {
             resultCode?: string;
@@ -292,13 +292,13 @@ export interface components {
         };
         MemberWithUsernameDto: {
             /** Format: int64 */
-            id?: number;
+            id: number;
             /** Format: date-time */
-            createDate?: string;
+            createDate: string;
             /** Format: date-time */
-            modifyDate?: string;
-            nickname?: string;
-            username?: string;
+            modifyDate: string;
+            nickname: string;
+            username: string;
         };
         PostDto: {
             /** Format: int64 */
