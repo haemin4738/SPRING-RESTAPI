0a43139ae92e6176aa24bf9f60954b482077ba18
haemin4738 <leeheamin12@gmail.com>
2025-09-25 10:32:56 +0900

    댓글, 게시글 삭제 openapi-fetch

--- frontend/src/app/posts/[id]/page.tsx
+++ frontend/src/app/posts/[id]/page.tsx
@@ -36,17 +36,28 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
   const deletePostComment = (id: number, commentId: number) => {
     if (!confirm('정말 삭제하시겠습니까?')) return
 
-    apiFetch(`/api/v1/posts/${id}/comments/${commentId}`, {
-      method: 'DELETE',
-    }).then((data) => {
-      alert(data.msg)
+    client
+      .DELETE('/api/v1/posts/{postId}/comments/{id}', {
+        params: {
+          path: {
+            postId: id,
+            id: commentId,
+          },
+        },
+      })
+      .then((res) => {
+        if (res.error) {
+          alert(res.error.msg)
+          return
+        }
+        alert(res.data.msg)
 
-      if (postComments === null) return
+        if (postComments === null) return
 
-      setPostComments(
-        postComments.filter((comment) => comment.id !== commentId),
-      )
-    })
+        setPostComments(
+          postComments.filter((comment) => comment.id !== commentId),
+        )
+      })
   }
 
   const handleSumbit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -64,19 +75,30 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
       return
     }
 
-    apiFetch(`/api/v1/posts/${id}/comments`, {
-      method: 'POST',
-      body: JSON.stringify({
-        content: contentInput.value,
-      }),
-    }).then((data) => {
-      alert(data.msg)
-      contentInput.value = ''
+    client
+      .POST('/api/v1/posts/{postId}/comments', {
+        params: {
+          path: {
+            postId: id,
+          },
+        },
+        body: {
+          content: contentInput.value,
+        },
+      })
+      .then((res) => {
+        if (res.error) {
+          alert(res.error.msg)
+          return
+        }
 
-      if (postComments == null) return
+        alert(res.data.msg)
+        contentInput.value = ''
 
-      setPostComments([...postComments, data.data])
-    })
+        if (postComments == null) return
+
+        setPostComments([...postComments, res.data.data])
+      })
   }
 
   useEffect(() => {
