69b9d300c4521462babc6e5ba46267ecd14c6edb
haemin4738 <leeheamin12@gmail.com>
2025-09-25 11:30:22 +0900

    apifetch 함수 삭제

--- frontend/src/lib/backend/client.ts
+++ frontend/src/lib/backend/client.ts
@@ -10,22 +10,3 @@ export const client = createClient<paths>({
   baseUrl : API_BASE_URL,
   credentials : "include"
 })
-
-
-export const apiFetch = (url: string, options?: RequestInit) => {
-  options = options || {};
-  options.credentials = "include";
-
-  if (options?.body) {
-    const headers = new Headers(options.headers || {});
-
-    // 이미 호출하는 쪽에서 Content-Type을 지정했다면 덮어쓰지 않고 그대로 사용
-    if (!headers.has("Content-Type")) {
-      headers.set("Content-Type", "application/json");
-    }
-
-    options.headers = headers;
-  }
-
-  return fetch(`${API_BASE_URL}${url}`, options).then((res) => res.json());
-};
