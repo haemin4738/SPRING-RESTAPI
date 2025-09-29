dda5a94617320973668bbd79df4b556d499178e7
haemin4738 <leeheamin12@gmail.com>
2025-09-29 12:26:11 +0900

    3번 회원 소셜로그인 인증쿠키 생성

--- backend/src/main/java/com/back/global/Rq/Rq.java
+++ backend/src/main/java/com/back/global/Rq/Rq.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -68,10 +69,11 @@ public class Rq {
         Cookie cookie = new Cookie(name, value);
         cookie.setPath("/"); // 쿠키를 도메인 전체에서 쓰겠다.
         cookie.setHttpOnly(true); // 쿠키를 스크립트로 접근 못하게(XSS 공격방어)
-        cookie.setDomain("localhost");
-        cookie.setSecure(true);
-        cookie.setAttribute("sameSite","Strict");
+        cookie.setDomain("localhost"); // 쿠키가 적용될 도메인 지정
+        cookie.setSecure(true); // https 에서만 쿠키전송
+        cookie.setAttribute("SameSite", "Strict"); // 동일 사이트에서만 쿠키 전송(CSRF 공격방어)
 
+        // 값이 없다면 해당 변수를 삭제하라는 뜻
         if (value.isBlank()) {
             cookie.setMaxAge(0);
         } else {
@@ -83,5 +85,10 @@ public class Rq {
 
     public void deleteCookie(String name) {
         setCookie(name, null);
+    }
+
+    @SneakyThrows
+    public void sendRedirect(String url) {
+        resp.sendRedirect(url);
     }
 }
